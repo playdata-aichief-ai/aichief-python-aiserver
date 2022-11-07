f3c905bd26bac07ac0a48da2554b2e9d5e3424e9
Zolyer <101810598+Zolyer@users.noreply.github.com>
2022-11-07 17:11:26 +0900

    Bbox resize 로직 추가

--- preprocessing/box_detection.py
+++ preprocessing/box_detection.py
@@ -1,6 +1,7 @@
 import json
 import os
 from ai.settings.settings import BASE_DIR
+import cv2
 
 
 class Box_Detection():
@@ -30,8 +31,12 @@ class Box_Detection():
                 w = j['bbox'][2]
                 h = j['bbox'][3]
 
-                box_image = cropped_image[int(
-                    y): int(y+h), int(x): int(x+w)]
+                box_image = cropped_image[int(y): int(y+h), int(x): int(x+w)]
+                
+                if box_image.shape[0] < 32:
+                    y_per = 32/box_image.shape[0]
+                    # box_image = cv2.resize(box_image, dsize=(0, 0), fx=y_per, fy=y_per, interpolation=cv2.INTER_AREA)
+                    box_image = cv2.resize(box_image, dsize=(0, 0), fx=y_per, fy=y_per, interpolation=cv2.INTER_LANCZOS4)
 
                 self.image_dict[f'{column_name}'] = box_image
 
