0131c3027bfdc3100ff32cfb6a44f197073e6bfd
cho2 <chso1324@naver.com>
2022-11-09 09:36:47 +0900

    Return Data Format 변경 및 Text_recognition 모델 변경. (이전 어절 단위 학습 모델로 변경)

--- controller/views.py
+++ controller/views.py
@@ -31,6 +31,45 @@ class GetInformation(APIView):
     permission_classes = [IPBasedPermission]
     serializer_class = RequestedSerializer
 
+    def convert_value_format_from_key(self, result, k, v):
+        if "SocialSecurityNumber" in k:
+            try:
+                v2 = result[k] + v
+                result[k] = v2
+            except:
+                v2 = v + "-"
+                result[k] = v2
+
+        elif "PhoneNumber" in k:
+            try:
+                v2 = result[k] + "-" + v
+                result[k] = v2
+            except:
+                v2 = v + "-"
+                result[k] = v2
+        elif "Email" in k:
+            try:
+                v2 = result[k] + "." + v
+                result[k] = v2
+            except:
+                v2 = v + "@"
+                result[k] = v2
+        elif "DateTime" in k:
+            try:
+                v2 = result[k] + "-" + v
+                result[k] = v2
+            except:
+                v2 = v + "-"
+                result[k] = v2
+        else:
+            try:
+                v2 = result[k] + " " + v
+                result[k] = v2
+            except:
+                v2 = v
+                result[k] = v2
+        return
+
     def get_image_from_s3(self, img_path, download=False):
         bucket = AWS_STORAGE_BUCKET_NAME
         region = S3DIRECT_REGION
@@ -137,11 +176,11 @@ class GetInformation(APIView):
 
             result = {}
             for i in range(len(result_key)):
-                v = result.get(result_key[i]) or []
-                v.append(recogntion_result[i])
-                result[result_key[i]] = v
-            # result[img_key[i]] = ControllerConfig.tr.predict(
-            #     file_name + str(i), [Image.fromarray(target) for target in yolo_cropped_img_dic[img_key[i]]])[0]
+                self.convert_value_format_from_key(
+                    result, result_key[i], recogntion_result[i])
+                # v = result.get(result_key[i]) or ""
+                # v += " " + recogntion_result[i]
+                # result[result_key[i]] = v
 
             res = Responsed(user=user, contractId=contract_id,
                             imagePath=image_path, result=result)
@@ -187,124 +226,3 @@ class ClickProcessLog(APIView):
             pl.save()
             return Response(status=status.HTTP_200_OK)
         return Response({'Processes Not Found': 'Invalid Request'}, status=status.HTTP_400_BAD_REQUEST)
-
-
-################ no use########################
-        # 1번 방식: AI Hub Label 데이터 추출해서 Detection 하는 형식으로
-        # 추출 -> static/images에 영역별로 분리해서 저장
-        # 2번 방식: Text Detection demo버젼(손글씨, 인쇄글씨 모두 검출)
-
-        # # 1번 방식
-        # try:
-        #     with open(os.path.join(BASE_DIR, 'static', 'json', file_name + '.json'), 'r', encoding='utf8') as f:
-        #         json_files = json.load(f)
-        #     img_info = json_files['images'][0]
-
-        #     # img = cv2.imread(image, cv2.IMREAD_COLOR)
-        #     targets = json_files['annotations'][0]['polygons']
-
-        #     for i, l in enumerate(targets):
-        #         if l['type'] != int(2):
-        #             continue
-        #         l['points'] = sorted(
-        #             l['points'], key=lambda x: (x[0], x[1]))
-        #         d1 = list(map(int, l['points'][0]))
-        #         d2 = list(map(int, l['points'][3]))
-        #         try:
-        #             sliced_img = img[d1[1]:d2[1], d1[0]:d2[0]]  # 높이, 너비
-        #             # sliced_img_name = img_info['identifier'] + \
-        #             #     f'_{i}.' + img_info['type']
-
-        #             # ndarray -> 이미지로 다시 전환해서 바로 보내기. (서버에 나눈 이미지 저장 후 불러오는게 X)
-        #             pred_img.append(Image.fromarray(sliced_img))
-        #             coordinates.append(l['points'])
-        #             # cv2.imwrite(os.path.join(BASE_DIR, 'static',
-        #             #             'images', image, sliced_img_name), sliced_img)
-        #         except:
-        #             pass
-
-        # # 2번 방식
-        # except:
-        #     targets, _, _ = ControllerConfig.td.predict(img)
-
-        #     for i, l in enumerate(targets):
-        #         # if l['type'] != int(2):
-        #         #     continue
-        #         # l['points'] = sorted(l['points'], key=lambda x: (x[0], x[1]))
-        #         # d1 = list(map(int, l['points'][0]))
-        #         # d2 = list(map(int, l['points'][3]))
-
-        #         l = sorted(l, key=lambda x: (x[0], x[1]))
-        #         d1 = list(map(int, l[0]))
-        #         d2 = list(map(int, l[3]))
-        #         try:
-        #             sliced_img = img[d1[1]:d2[1], d1[0]:d2[0]]  # 높이, 너비
-        #             # sliced_img_name = img_info['identifier'] + \
-        #             #     f'_{i}.' + img_info['type']
-
-        #             # ndarray -> 이미지로 다시 전환해서 바로 보내기. (서버에 나눈 이미지 저장 후 불러오는게 X)
-        #             pred_img.append(Image.fromarray(sliced_img))
-        #             coordinates.append(l)
-        #             # cv2.imwrite(os.path.join(BASE_DIR, 'static',
-        #             #             'images', image, sliced_img_name), sliced_img)
-        #         except:
-        #             pass
-
-
-# 이미지에 결과 그려서 확인. local 작업시 편의 위한 기능. 배포시 주석
-        # try:
-        #     img2 = np.ones((img.shape[0], img.shape[1], 3), np.uint8) * 255
-        #     for i in range(len(coordinates)):
-        #         points = np.array([list(map(int, p))
-        #                            for p in coordinates[i]]).astype(np.int32)
-        #         txt = result[0]['result'][0][i]
-        #         center = [(points[3][0] + points[0][0]) // 2,
-        #                   (points[3][1] + points[0][1]) // 2]
-        #         m = points[3][0] - center[0]
-        #         area = ((points[3][0] - points[0][0]) *
-        #                 (points[3][1] - points[0][1])) // 150
-        #         cv2.rectangle(img, points[0], points[3],
-        #                       (255, 0, 255), 2, cv2.LINE_AA)
-        #         b, g, r, a = 100, 100, 250, 0
-        #         fontpath = "fonts/gulim.ttc"
-        #         font = ImageFont.truetype(fontpath, int(area))
-        #         img_pil = Image.fromarray(img2)
-        #         draw = ImageDraw.Draw(img_pil)
-        #         draw.text((center[0], center[1]),
-        #                   txt, font=font, fill=(b, g, r, a))
-        #         img2 = np.array(img_pil)
-
-        #     img = cv2.resize(img, (800, 1000))
-        #     img2 = cv2.resize(img2, (800, 1000))
-
-        #     cv2.imshow('img', img)  # [:, :, ::-1])
-        #     cv2.imshow('img2', img2)
-        #     cv2.waitKey(0)
-        #     cv2.destroyAllWindows()
-        # except:
-        #     pass
-
-        # # Text Detection
-        # for k in img_key:
-        #     re_cropped_img = self.re_crop_detection(sr_img_dic[k])
-        #     try:
-        #         re_cropped_img = Image.fromarray(re_cropped_img)
-        #     except:
-        #         re_cropped_img = Image.fromarray(sr_img_dic[k])
-        #     img_values.append(re_cropped_img)
-
-
-# Text Detection
-        # for k in img_key:
-        #     re_cropped_img = self.re_crop_detection(sr_img_dic[k])
-        #     try:
-        #         re_cropped_img = Image.fromarray(re_cropped_img)
-        #     except:
-        #         re_cropped_img = Image.fromarray(sr_img_dic[k])
-        #     img_values.append(re_cropped_img)
-
-        # crop_img = self.re_crop_detection(img)
-        # try:
-        #     crop_img = list(map(Image.fromarray, crop_img))
-        # except:
-        #     crop_img = [Image.fromarray(img)]
